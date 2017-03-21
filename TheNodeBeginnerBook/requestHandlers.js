78d6b413358a9b28f744a952ed24cd7a8790881e
Darpan Dodiya <darpandodiya@gmail.com>
2017-03-21 20:28:29 +0530

    Forgot to add this.

--- TheNodeBeginnerBook/requestHandlers.js
+++ TheNodeBeginnerBook/requestHandlers.js
@@ -1,5 +1,6 @@
 var querystring = require("querystring"),
-    fs = require("fs");
+    fs = require("fs"),
+    formidable = require("formidable");
 
 function start(response, postData) {
     console.log("Request handler 'start' was called.");
@@ -23,11 +24,26 @@ function start(response, postData) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log("Request handler 'upload' was called.");
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("You've sent: " + querystring.parse(postData).text);
+    var form = new formidable.IncomingForm();
+    console.log("About to parse");
+    form.parse(request, function(error, fields, files) {
+        console.log("parsing done");
+
+        /* Possible error on Windows systems:
+        tried to rename to an already existing file */
+        fs.rename(files.upload.path, "tmp/test.png", function(error) {
+            if (error) {
+                fs.unlink("tmp/test.png");
+                fs.rename(files.upload.path, "/tmp/test.png");
+            }
+        });
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("received image:<br/>");
+    response.write("<img src='/show' />");
     response.end();
+    });
 }
 
 function show(response) {
@@ -38,4 +54,4 @@ function show(response) {
 
 exports.start = start;
 exports.upload = upload;
-exports.show = show
+exports.show = show;
